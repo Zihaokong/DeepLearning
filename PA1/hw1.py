09d00b757306b7cb221256a67e649a622e6e5b31
Zihaokong <kongzihao99@gmail.com>
2020-10-12 20:19:47 -0700

    sd

--- PA1/hw1.py
+++ PA1/hw1.py
@@ -12,16 +12,23 @@ def normalize_axis(vect):
     vect = (vect-mi)/(ma-mi)
     return vect
 
-
-
-
+def PCA(A,p):
+    AT=A.transpose()
+    print(AT)
+    n = A.shape[0]
+    C=(AT.dot(A))
+    print(C,C.shape)
 onehot = np.zeros((y_test.size,max(y_test)+1))
 #Array of 10000*10
 onehot[np.arange(y_test.size),y_test] = 1
 #set corresponding position to 1
 
+a = np.array([[1,2,3],[4,5,6],[7,8,9]])
 
-X_train = np.apply_along_axis(normalize_axis, 0, X_train)
-X_test = np.apply_along_axis(normalize_axis, 0, X_test)
+PCA(X_train,5)
+
+
+#X_train = np.apply_along_axis(normalize_axis, 0, X_train)
+#X_test = np.apply_along_axis(normalize_axis, 0, X_test)
 
 
